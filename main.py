7764e0e21619647fa1b44c684f39eecc2ef49be6
chetanpachpohe <80163665+chetanpachpohe@users.noreply.github.com>
2021-03-06 20:51:53 +0530

    Add files via upload
    Committing changes for bookyourmovie.com. Where user can see seats in cinema hall , buy tickets , can see statistics , shows booked seats and user information (such as name ,age ,gender, contact no) and a exit option.

---
+++ main.py
@@ -0,0 +1,25 @@
+from book import Show
+global row
+global seats
+
+row=int(input("Enter the number of rows: "))
+seats=int(input("Enter the number of seats in each row: "))
+while True:
+    print("*** Welcome to Bookyourmovies ***")
+    catlog=input("1. show seats\n2.Buy tickets\n3.view statistics\n4. show booked tickets customer info\n0.exit\n\n")
+
+    obj=Show()
+    if catlog == "1":
+        obj.show_seats(row,seats)
+    elif catlog == "2":
+        obj.buy_tickets(row,seats)
+    elif catlog == "3":
+        obj.show_statistics(row,seats)
+    elif catlog =="4":
+        obj.show_booked_tickets()
+    elif catlog == "0":
+        break
+
+
+
+
